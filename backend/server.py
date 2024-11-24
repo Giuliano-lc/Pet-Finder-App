c7bf30e1d734847cb8d3291ce643ce813e0d3217
Gabriel Barbosa <92130323+GabrielBarbosaLK@users.noreply.github.com>
2024-11-24 17:12:21 -0300

    login/cadastro

---
+++ backend/server.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+import sqlite3
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
+app = Flask(__name__)
+
+CORS(app)
+
+app.config['JWT_SECRET_KEY'] = 'sua_chave_secreta_aqui'
+jwt = JWTManager(app)
+
+conn = sqlite3.connect("data.db")
+
+@app.route("/login", methods = ['POST', 'GET'])
+def login():
+    if request.method == "POST": 
+        data = request.get_json('nome')
+        conn = sqlite3.connect("data.db")
+        usuarios = conn.execute("SELECT * FROM usuario")
+        for i in usuarios:
+            if data['email'] == i[1]:
+                if data['senha'] == i[2]:
+                    access_token = create_access_token(identity=data["email"])
+                    return jsonify(access_token=access_token)
+        return jsonify({'status': 'Usuario ou senha invalidos'})
+
+@app.route("/cadastro", methods = ["GET", "POST"])
+def cadastro():
+    if request.method == "POST":
+        conn = sqlite3.connect("data.db")
+        data = request.get_json('nome')
+        emails = conn.execute(f"SELECT email FROM usuario")
+        for i in emails:
+            if i[0] == data['email']:
+                return jsonify({'status': 'Email ja existe'})
+
+        conn.execute(f"INSERT INTO usuario (nome, email, senha) VALUES ('{data['nome']}', '{data['email']}', '{data['senha']}')")
+        conn.commit()
+
+        return jsonify({'status': 'Cadastrato com sucesso'})
